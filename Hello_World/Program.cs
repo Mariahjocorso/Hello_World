c32d90d68b0684df69e74727b36b3e44a3ba50c3
Mariah Jo Corso <mariahjocorso@gmail.com>
2023-01-23 14:59:53 -0500

    Hello_World

---
+++ Hello_World/Program.cs
@@ -0,0 +1,43 @@
+﻿//Code from first interactive program lesson for Code Louisville
+//step one
+Console.WriteLine("Hello, World!");
+
+//step 2
+string aFriend = "Bill";
+Console.WriteLine($"Hello {aFriend}");
+
+aFriend = "Maira";
+Console.WriteLine($"Hello {aFriend}");
+
+//step 3
+string firstFriend = "Maria";
+string secondFriend = "Sage";
+Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
+
+Console.WriteLine($"The name {firstFriend} has {firstFriend.Length} letters.");
+Console.WriteLine($"The name {secondFriend} has {secondFriend.Length} letters.");
+
+//step 4
+string greeting = "      Hello World!       ";
+Console.WriteLine($"[{greeting}]");
+
+string trimmedGreeting = greeting.TrimStart();
+Console.WriteLine($"[{trimmedGreeting}]");
+Console.WriteLine($"[{trimmedGreeting}]");
+
+string sayHello = "Hello World!";
+Console.WriteLine(sayHello);
+sayHello = sayHello.Replace("Hello", "Greetings");
+Console.WriteLine(sayHello);
+
+Console.WriteLine(sayHello.ToUpper());
+Console.WriteLine(sayHello.ToLower());
+
+//step 5
+string songLyrics = "You say goodbye, and I say hello";
+Console.WriteLine(songLyrics.Contains("goodbye"));
+Console.WriteLine(songLyrics.Contains("greetings"));
+
+//challenge
+Console.WriteLine(songLyrics.StartsWith("goodbye"));
+Console.WriteLine(songLyrics.EndsWith("hello"));
